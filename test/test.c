069a55130f4d855968e933c07b09651b4cdb05be
qianfeng <qianfeng@TeacherRuth.local>
2015-12-11 17:56:18 +0800

    second

---
+++ test/test.c
@@ -0,0 +1,9 @@
+//
+//  test.c
+//  test
+//
+//  Created by qianfeng on 15/12/11.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#include "test.h"
---
+++ test/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  test
+//
+//  Created by qianfeng on 15/12/11.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#ifndef __test__test__
+#define __test__test__
+
+#include <stdio.h>
+
+#endif /* defined(__test__test__) */
